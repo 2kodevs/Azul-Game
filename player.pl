6349e715b167d71a73cc124a1cff8366c25def03
Lazaro Raul <riglesiasvera@gmail.com>
2020-03-30 22:18:43 -0400

    refs #5 - Find all valid choices in the player turn
    Method `valid_choices(<game>, <player>, <choices>)`

--- player.pl
+++ player.pl
@@ -4,3 +4,16 @@ column_of(Line, Color, Column):-
     tiles_colors(Colors),
     index_of(Color, Colors, Idx),
     Column is ((Idx + Line - 1) mod 5) + 1.
+
+valid_choices(Game, Player, C):-
+    property_of(factories, Game, Fac),
+    property_of(board, Player, Board),
+    findall(Lid:Fid:Color, (
+        property_of(Lid, Board, Line),
+        property_of(stocks, Line, Stocks),
+        member(empty, Stocks),
+        property_of(valid, Line, ValidColors),
+        property_of(Fid, Fac, CurFac),
+        member(Color, ValidColors),
+        member(Color, CurFac)
+    ), C).
